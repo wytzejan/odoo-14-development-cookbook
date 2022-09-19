a8d2dc7ecbf8618dc3673c596b2af04c296d3e7f
wytzejan <wjriedstra@gmail.com>
2022-09-19 19:12:56 +0200

    Add local-addons and config

---
+++ local-addons/my_library/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'My Library',
+    'summary': 'Manage books easily',
+    'description': 'Manage Library',
+    'author': 'Wytze Jan Riedstra',
+    'website': 'https://www.example.com',
+    'category': 'Uncategorized',
+    'version': '14.0.1',
+    'depends': ['base'],
+    'data': [
+        'security/groups.xml',
+        'security/ir.model.access.csv',
+        'views/library_book.xml'
+    ],
+    'demo': ['demo.xml']
+}
---
+++ local-addons/my_library/models/library_book.py
@@ -0,0 +1,34 @@
+from importlib.metadata import requires
+from unicodedata import digit
+from odoo import models, fields
+
+
+class LibraryBook(models.Model):
+    _name = 'library.book'
+    _description = 'Library Book'
+    _order = 'date_release desc, name'
+    _rec_name = 'short_name'
+    name = fields.Char('Title', required=True)
+    short_name = fields.Char('Short Title', required=True)
+    notes = fields.Text('Internal Notes')
+    state = fields.Selection(
+        [('draft', 'Not Available'),
+         ('available', 'Available'),
+         ('lost', 'Lost')],
+        'State')
+    description = fields.Html('Description')
+    cover = fields.Binary('Book Cover')
+    out_of_print = fields.Boolean('Out of Print?')
+    date_release = fields.Date('Release Date')
+    date_updated = fields.Datetime('Last Updated')
+    pages = fields.Integer('Number of Pages')
+    reader_rating = fields.Float(
+        'Reader Average Rating',
+        digits=(14, 4),  # Optional precision decimals
+    )
+    author_ids = fields.Many2many(
+        'res.partner',
+        string='Authors'
+    )
+    cost_price = fields.Float(
+        'Book Cost', digits='Book Price')
