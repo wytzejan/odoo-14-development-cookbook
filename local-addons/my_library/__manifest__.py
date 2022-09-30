0486866ef6655507a4ac11ff432db6b56df04caf
Wytze Jan Riedstra <wjriedstra@gmail.com>
2022-09-30 19:30:40 +0200

    Loading data using XML files

--- local-addons/my_library/__manifest__.py
+++ local-addons/my_library/__manifest__.py
@@ -12,6 +12,7 @@
         'security/ir.model.access.csv',
         'views/library_book.xml',
         'views/library_book_categ.xml',
-        'data/data.xml'
+        'data/data.xml',
+        'data/demo.xml'
     ],
 }
